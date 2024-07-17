bfaefa27ba9f706ff9f8c262cb94b7977b469b12
Shrey Kariria <115896460+lmly303@users.noreply.github.com>
2024-07-17 16:21:19 +0530

    uploading sql database;

---
+++ shipping_logistics.sql
@@ -0,0 +1,15 @@
+-- Active: 1721017226150@@localhost@3306@shipping_logistics
+desc `Billing`;
+desc `Delivery`;
+desc `Package`;
+DESC `Receiver`;
+DESC `Sender`;
+desc `Tracking`;
+SELECT * from `Billing`;
+SELECT * from `Delivery`;
+SELECT * from `Package`;
+SELECT * FROM `Receiver`;
+SELECT * FROM `Sender`;
+SELECT * FROM `Tracking`;
+
+
